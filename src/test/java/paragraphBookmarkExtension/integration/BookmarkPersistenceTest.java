606a8c95ec76a40a14223c140699dc81f809bc42
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2016-10-24 21:30:32 +0300

    added tests for bookmark service

--- src/test/java/paragraphBookmarkExtension/integration/BookmarkPersistenceTest.java
+++ src/test/java/paragraphBookmarkExtension/integration/BookmarkPersistenceTest.java
@@ -11,10 +11,12 @@ import paragraphBookmarkExtension.service.UserService;
 import paragraphBookmarkExtension.transfer.exception.GenericException;
 import paragraphBookmarkExtension.transfer.user.UserTO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
@@ -80,5 +82,84 @@ public class BookmarkPersistenceTest {
             assertThat(searchedBookmark.getUserId(), is(bookmark.getUserId()));
             assertThat(searchedBookmark.getDocumentUrl(), is(bookmark.getDocumentUrl()));
         }
+    }
+
+    @Test
+    public void deleteBookmarkById() throws GenericException {
+        String email = "test" + System.currentTimeMillis() + "@example.com";
+        String password = "password";
+        UserTO user = userService.createUser(email, password, false);
+        assertThat("Failed to create user.", user, notNullValue());
+
+        String documentUrl = "http://example.com";
+        String textFirstPart = System.currentTimeMillis() + "";
+        String textLastPart = System.currentTimeMillis() + "";
+        Bookmark bookmark = bookmarkService.addBookmark(user.getId(), documentUrl, textFirstPart, textLastPart);
+        assertThat("Failed to create bookmark.", bookmark, notNullValue());
+
+        bookmarkService.deleteBookmark(bookmark.getId());
+
+        Bookmark deletedBookmark = bookmarkService.getBookmark(bookmark.getId());
+        assertThat("Deleted bookmark unexpectedly retrieved from DB.", deletedBookmark, nullValue());
+    }
+
+    @Test
+    public void batchDeleteBookmarksById() throws GenericException {
+        String email = "test" + System.currentTimeMillis() + "@example.com";
+        String password = "password";
+        UserTO user = userService.createUser(email, password, false);
+        assertThat("Failed to create user.", user, notNullValue());
+
+        List<Long> bookmarkIdsToBeDeleted = new ArrayList<>();
+        List<Long> bookmarkIdsToBeSaved = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            String documentUrl = "http://example" + System.currentTimeMillis() + ".com";
+            String textFirstPart = System.currentTimeMillis() + "";
+            String textLastPart = System.currentTimeMillis() + "";
+            Bookmark bookmark = bookmarkService.addBookmark(user.getId(), documentUrl, textFirstPart, textLastPart);
+            assertThat("Failed to create bookmark.", bookmark, notNullValue());
+            if (i < 3) {
+                bookmarkIdsToBeDeleted.add(bookmark.getId());
+            } else {
+                bookmarkIdsToBeSaved.add(bookmark.getId());
+            }
+        }
+
+        bookmarkService.batchDeleteBookmarks(bookmarkIdsToBeDeleted);
+
+        for (Long bookmarkId : bookmarkIdsToBeDeleted) {
+            Bookmark deletedBookmark = bookmarkService.getBookmark(bookmarkId);
+            assertThat("Deleted bookmark unexpectedly retrieved from DB.", deletedBookmark, nullValue());
+        }
+        for (Long bookmarkId : bookmarkIdsToBeSaved) {
+            Bookmark remainingBookmark = bookmarkService.getBookmark(bookmarkId);
+            assertThat("Bookmark unexpectedly deleted.", remainingBookmark, notNullValue());
+        }
+    }
+
+    @Test
+    public void batchDeleteAllBookmarksByUserId() throws GenericException {
+        String email = "test" + System.currentTimeMillis() + "@example.com";
+        String password = "password";
+        UserTO user = userService.createUser(email, password, false);
+        assertThat("Failed to create user.", user, notNullValue());
+
+        List<Long> bookmarkIds = new ArrayList<>();
+        int bookmarksToBeCreated = 3;
+        for (int i = 0; i < bookmarksToBeCreated; i++) {
+            String documentUrl = "http://example" + System.currentTimeMillis() + ".com";
+            String textFirstPart = System.currentTimeMillis() + "";
+            String textLastPart = System.currentTimeMillis() + "";
+            Bookmark bookmark = bookmarkService.addBookmark(user.getId(), documentUrl, textFirstPart, textLastPart);
+            assertThat("Failed to create bookmark.", bookmark, notNullValue());
+            bookmarkIds.add(bookmark.getId());
+        }
+
+        int bookmarksCount = bookmarkService.countByUserId(user.getId());
+        assertThat(bookmarksCount, is(bookmarksToBeCreated));
+
+        bookmarkService.deleteAllBookmarksOfOneUser(user.getId());
+        bookmarksCount = bookmarkService.countByUserId(user.getId());
+        assertThat(bookmarksCount, is(0));
     }
 }
