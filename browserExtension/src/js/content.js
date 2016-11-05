dc43bdf14beed7eb32339b95bc230295613c6e26
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2016-11-05 22:35:39 +0200

    added highlighting and specs extraction

--- browserExtension/src/js/content.js
+++ browserExtension/src/js/content.js
@@ -6,11 +6,14 @@
     forge.message.listen(
        "highlight_triggered",
        function() {
-        console.log("'highlight_triggered' message received.")
-        createMark();
+            console.log("'highlight_triggered' message received.");
+            $("body :not(:button, img, script, img)").mouseup(function(e) {
+                var markSpecs = getMarkSpecs();
+                createMark(markSpecs);
+            });
        },
        function(content) {
-        console.log(content)
+            console.log(content);
        }
     );
  });
--- browserExtension/src/js/marker.js
+++ browserExtension/src/js/marker.js
@@ -2,14 +2,10 @@
  * Created by Flaviu Ratiu on 10/26/2016.
  */
 
- function createMark() {
+ function createMark(markSpecs) {
      var documentUrl = "loc";
-     var text = "ab e";
-     var precedingText = "ab";
-     var trailingText = "b";
      var emailReminderEnabled = true;
      var userId = 1;
-     // TODO: extract and parse text parts
      // TODO: extract current url
      // TODO: get user ID from cookie
 
@@ -19,9 +15,9 @@
          data: {
              userId: userId,
              documentUrl: documentUrl,
-             text: text,
-             precedingText: precedingText,
-             trailingText: trailingText,
+             text: markSpecs.selected,
+             precedingText: markSpecs.before,
+             trailingText: markSpecs.after,
              emailReminderEnabled: emailReminderEnabled
          },
          dataType: "json",
@@ -29,10 +25,63 @@
      })
      .done(function() {
          console.log("Highlighting saved.");
-         // TODO: Display bookmark
+         highlightText(markSpecs.before, markSpecs.selected, markSpecs.after);
      })
      .fail(function() {
          console.log("Failed to save highlighting.");
          // TODO: Bookmark creation failure message
      })
+ }
+
+ function highlightText(precedingText, text, trailingText) {
+    var regex = new RegExp("(" + precedingText + ")(" + text + ")(" + trailingText + ")","gim");
+    $("body").markRegExp(regex, {
+        "acrossElements": true,
+        "ignoreGroups": 1,
+        "caseSensitive": true,
+        "iframes": true
+    });
+ }
+
+ function getMarkSpecs() {
+    var sel, range, tempRange, containerEl = null, before = "", after = "";
+    if (typeof window.getSelection != "undefined") {
+        sel = window.getSelection();
+        if (sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            containerEl = range.commonAncestorContainer;
+            if (containerEl.nodeType != 1) {
+                containerEl = containerEl.parentNode;
+            }
+        } else {
+            range = document.createRange();
+            range.collapse(true);
+        }
+        tempRange = document.createRange();
+        tempRange.selectNodeContents(containerEl);
+        tempRange.setEnd(range.startContainer, range.startOffset);
+        before = tempRange.toString();
+
+        tempRange.selectNodeContents(containerEl);
+        tempRange.setStart(range.endContainer, range.endOffset);
+        after = tempRange.toString();
+
+        tempRange.detach();
+    } else if ( (sel = document.selection) && sel.type != "Control") {
+        range = sel.createRange();
+        containerEl = range.parentElement();
+        tempRange = document.body.createTextRange();
+        tempRange.moveToElementText(containerEl);
+        tempRange.setEndPoint("EndToStart", range);
+        before = tempRange.text;
+
+        tempRange.moveToElementText(containerEl);
+        tempRange.setEndPoint("StartToEnd", range);
+        after = tempRange.text;
+    }
+    return {
+        before: before,
+        selected: sel.toString(),
+        after: after
+    };
  }
