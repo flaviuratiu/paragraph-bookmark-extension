21ca28bdc459305cc5d53b01c84afcec6007ff9d
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2016-11-09 09:50:40 +0200

    added success and error messages on trigger highlighting action

--- browserExtension/src/js/popup.js
+++ browserExtension/src/js/popup.js
@@ -3,16 +3,27 @@
  */
 
  function broadcastHighlightMessage() {
+    var errorMessage = $(".error-message");
+    var successMessage = $(".success-message");
     forge.message.toFocussed(
         "highlight_triggered",
         "Capture next selection.",
-        function(){console.debug("Highlight done successfully.")},
-        function(){console.debug("Failed to trigger highlighting.")
-    })
+        function(){
+            console.debug("Highlight done successfully.");
+        },
+        function(){
+            console.debug("Failed to trigger highlighting.");
+            errorMessage.text("Failed to trigger highlighting. Please try again later.");
+            errorMessage.show();
+    });
+    if ($(errorMessage).is(":hidden")) {
+        successMessage.text("You can now select the text for highlighting.");
+        successMessage.show();
+    }
   }
 
  $(document).ready(function() {
-     var cookie = Cookies.get("permanent-marker-extension");
+     var cookie = Cookies.getJSON("permanent-marker-extension");
      if (cookie && cookie.user) {
         $("#log-in").hide();
         $("#manage-account").show();
@@ -24,8 +35,8 @@
              if (cookie && cookie.user) {
                 broadcastHighlightMessage();
              } else {
-                $("#error-message").text("Please log in to the Permanent Marker app.");
-                $("#error-message").show();
+                $(".error-message").text("Please log in before highlighting text.");
+                $(".error-message").show();
              }
          }
      );
