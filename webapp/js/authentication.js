ae93e0ad0e0218df9438762ffb7a8819f022ee08
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2016-11-09 03:03:28 +0200

    added authentication validations

--- webapp/js/authentication.js
+++ webapp/js/authentication.js
@@ -6,62 +6,101 @@ function signUp() {
     console.debug("Sign up flow started");
     email = $("#email").val();
     password = $("#password").val();
-    validateSignUpForm(email, password);
 
-    $.ajax({
-        method: 'POST',
-        url: window.apiHost + "/users/create",
-        data: {
-            email: email,
-            password: password
-        },
-        dataType: 'json',
-        crossDomain: true
-    })
-    .done(function(user) {
-        window.location.href = window.uiBasePath + "/account.html";
-        Cookies.set(window.appCookieName, {
-                user: user.id
-            });
-    })
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        console.debug("Sign up failed.\n" + jqXHR + " " + textStatus + "\n" + errorThrown);
-    })
+    if (validateSignUpForm(email, password)) {
+        $.ajax({
+            method: 'POST',
+            url: window.apiHost + "/users/create",
+            data: {
+                email: email,
+                password: password
+            },
+            dataType: 'json',
+            crossDomain: true
+        })
+        .done(function(user) {
+            window.location.href = window.uiBasePath + "/account.html";
+            Cookies.set(window.appCookieName, {
+                    user: user.id
+                });
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            console.debug("Sign up failed.\n" + jqXHR + " " + textStatus + "\n" + errorThrown);
+        })
+    }
 }
 
 function login() {
     email = $("#email").val();
     password = $("#password").val();
-    validateLoginForm(email, password);
 
-    $.ajax({
-        method: 'POST',
-        url: window.apiHost + "/users/login",
-        data: {
-            email: email,
-            password: password
-        },
-        dataType: 'json',
-        crossDomain: true
-    })
-    .done(function(user) {
-        window.location.href = window.uiBasePath + "/account.html";
-        Cookies.set(
-             window.appCookieName, {
-             user: user.id
-        });
-    })
-    .fail(function(jqXHR, textStatus, errorThrown) {
-        console.debug("Login failed.\n" + jqXHR + " " + textStatus + "\n" + errorThrown);
-    })
+    if (validateLoginForm(email, password)) {
+        $.ajax({
+            method: 'POST',
+            url: window.apiHost + "/users/login",
+            data: {
+                email: email,
+                password: password
+            },
+            dataType: 'json',
+            crossDomain: true
+        })
+        .done(function(user) {
+            window.location.href = window.uiBasePath + "/account.html";
+            Cookies.set(
+                 window.appCookieName, {
+                 user: user.id
+            });
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            console.debug("Login failed.\n" + jqXHR + " " + textStatus + "\n" + errorThrown);
+        })
+    }
 }
 
 function validateSignUpForm(email, password) {
-    // TODO: signup validations
+    var errorMessage = $(".error-message");
+    var confirmPasswordField = $("#confirm-password");
+
+    confirmPasswordField.click(function() {
+        errorMessage.hide();
+    })
+
+    if (!validateLoginForm(email, password)) {
+        return false;
+    }
+
+    if (confirmPasswordField.val() != password) {
+        errorMessage.text("Passwords do not match.");
+        errorMessage.show();
+        return false;
+    }
+    return true;
 }
 
 function validateLoginForm(email, password) {
-    // TODO: login validations
+    var errorMessage = $(".error-message");
+
+    $("#email").click(function() {
+        errorMessage.hide();
+    })
+    $("#password").click(function() {
+        errorMessage.hide();
+    })
+
+    var validEmail = email.match("^(\\w+)([\\-+.\\'][\\w]+)*@(\\w[\\-\\w]*\\.){1,5}([A-Za-z]){2,6}$");
+    if (!validEmail) {
+        errorMessage.text("Please fill in a valid e-mail address.");
+        errorMessage.show();
+        return false;
+    }
+
+    if (!password || password == "") {
+        errorMessage.text("Please fill in your password.");
+        errorMessage.show();
+        return false;
+    }
+    return true;
 }
 
 $(document).ready(function() {
